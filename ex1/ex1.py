a1f0b84afd14d1ba2816d9af73fbaff0d790faec
MarceloBD <marcelodianib@gmail.com>
2018-08-15 11:22:30 -0300

    TODO: use training cases together

--- ex1/ex1.py
+++ ex1/ex1.py
@@ -69,9 +69,68 @@ test6 = [[-1, 1,  1, -1,  1],
      [ 1, -1, -1, -1,  1]]
 testLabel = [1, 1, 1, -1, -1, -1]
 
-#function for printing matrix
-for x in range(0, 5):
-    for y in range(0, 5):
-    	print '{:>3}'.format(ai[x][y]),
-    print
+#weigths
+w = [[0, 0, 0, 0, 0],
+	 [0, 0, 0, 0, 0],
+	 [0, 0, 0, 0, 0],
+	 [0, 0, 0, 0, 0],
+	 [0, 0, 0, 0, 0]]
+
+eta = 0.5
+activate = 0
+counter = 0
+
+
+def activationFunc(value):
+	if (value >=0):
+		return 1
+	else:
+		return -1
+
+def printMatrix(matrix):
+	for x in range(0, 5):
+		for y in range(0, 5):
+			print '{:>3}'.format(matrix[x][y]),
+		print
+
+def processInput(inputMatrix):
+	output = 0
+	for x in range(0, 5):
+		for y in range(0, 5):
+			output = output + w[x][y]*inputMatrix[x][y]
+	return output
+	
+def isCorrectLabel(output, label):
+	global activate
+	activate = activationFunc(output)
+	if (label != activate):
+		return 0
+	else:
+		return 1
+
+def correctWeights(inputMatrix, label):
+	global w
+	for x in range(0, 5):
+		for y in range(0, 5):
+			w[x][y] = w[x][y] - eta*(activate-label)*inputMatrix[x][y]
+
+def perceptromAlgorithm(inputMatrix, label):
+	output = processInput(inputMatrix)
+	goodLabel = isCorrectLabel(output, label)
+	global counter
+	if(goodLabel != 1):
+		correctWeights(inputMatrix, label)
+		perceptromAlgorithm(inputMatrix, label)
+
+	else:
+		print output, activate
+
+
+# main program # 
+perceptromAlgorithm(a, trainLabel[4])
+printMatrix(w)
+
+
+
+
 
