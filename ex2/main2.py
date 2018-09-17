a57da1747d984a05c2508f75d6b8b717cd0e917a
Felipe Garcia <felipegarciaitu@hotmail.com>
2018-09-17 16:40:30 -0300

    Finished backpropagation

--- ex2/main2.py
+++ ex2/main2.py
@@ -3,7 +3,7 @@ from mult_layer_percepton import mlp
 
 if __name__ == '__main__':
     data = [1, 0, 0]
-    learn_rate = 0.5
-    epochs = 100
+    learn_rate = 0.1
+    epochs = 10000
     model = mlp(1, 3, 3, 3)
     model.backpropagation(data, data, epochs, learn_rate)
