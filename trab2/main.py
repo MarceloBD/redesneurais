743cafef997a47625750009c89ed2fa0c7b35089
Felipe Garcia <felipegarciaitu@hotmail.com>
2018-10-24 20:01:12 -0200

    Finished

--- trab2/main.py
+++ trab2/main.py
@@ -1,5 +1,7 @@
 from CNN import CNN
 import cv2
+import numpy as np
+import os
 
 
 names = {0: 'airplane',
@@ -14,11 +16,12 @@ names = {0: 'airplane',
          9: 'truck'}
 
 if __name__ == '__main__':
+    os.environ['CUDA_VISIBLE_DEVICES'] = str(0)
     num_classes = 10
     path = 'test_images/'
     img_size = 32
     model = CNN(num_classes)
-    # model.load('model.h5')
+    model.load('model.h5')
     images = []
     for key, value in names.items():
         img = cv2.imread(path + value + '.jpeg', cv2.IMREAD_COLOR)
@@ -26,3 +29,10 @@ if __name__ == '__main__':
         img = cv2.resize(img, (img_size, img_size))
         images.append(img)
     images = [image/255 for image in images]
+    images = np.reshape(images, (-1, img_size, img_size, 3))
+    predictions = model.recognize(images)
+    predictions = [np.argmax(predict) for predict in predictions]
+    for prediction in predictions:
+        prediction = names[prediction]
+        print("This is a", prediction)
+
