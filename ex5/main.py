d21ffb6f583e4c88f9a9510d4935fb0035b397bd
MarceloBD <marcelodianib@gmail.com>
2018-11-06 23:41:25 -0200

    Fized errors

--- ex5/main.py
+++ ex5/main.py
@@ -15,18 +15,21 @@ if __name__ == '__main__':
 	print(data, target)
 
 	pca = Pca()
-	cov = pca.cov_matrix(data[:,0], data[:,1], data[:,2])
+	cov = pca.cov_matrix(data[:,0], data[:,1], data[:,2], data[:,3])
 	
 	values, vectors = pca.eigen_values_vectors(cov)
-	pca.sort_eigen(values, vectors)
+
+	values, vectors = pca.sort_eigen(values, vectors)
+
+
+	vectors = pca.eigen_strip_vectors(values, vectors, 0.9)
+	values = values[:len(vectors[0])]
+	
+	print(pca.pca_result(data, vectors))
 
 
 
-
-
-	print(cov)
-
-	pca_result = decomposition.PCA(n_components=3)
+	pca_result = decomposition.PCA(n_components=4)
 	pca_result.fit(data)
 	points = pca_result.transform(data)
-	#print(points)
+	print(points)
--- ex5/pca.py
+++ ex5/pca.py
@@ -5,11 +5,12 @@ class Pca():
 	def __init__(self):
 		return
 
-	def cov_matrix(self, x, y, z):
+	def cov_matrix(self, x, y, z, l):
 		
-		c = [[np.cov(x, x)[0][1], np.cov(x,y)[0][1], np.cov(x,z)[0][1]],
-			[np.cov(y, x)[0][1], np.cov(y,y)[0][1], np.cov(y,z)[0][1]],
-			[np.cov(z, x)[0][1], np.cov(z,y)[0][1], np.cov(z,z)[0][1]]]
+		c = [[np.cov(x, x)[0][1], np.cov(x,y)[0][1], np.cov(x,z)[0][1], np.cov(x,l)[0][1]],
+			[np.cov(y, x)[0][1], np.cov(y,y)[0][1], np.cov(y,z)[0][1], np.cov(y,l)[0][1]],
+			[np.cov(z, x)[0][1], np.cov(z,y)[0][1], np.cov(z,z)[0][1], np.cov(z,l)[0][1]],
+			[np.cov(l, x)[0][1], np.cov(l,y)[0][1], np.cov(l,z)[0][1], np.cov(l,l)[0][1]]]
 		return c
 
 	def eigen_values_vectors(self, cov):
@@ -18,4 +19,20 @@ class Pca():
 	def sort_eigen(self, values, vectors):
 		ordered_index = np.argsort(-np.array(values))[:len(values)]
 		values_ordered = [values[i] for i in ordered_index]
-		print('v',values_ordered)
+		vectors_ordered = np.empty((4,1), float)
+		for i in ordered_index:
+			vectors_ordered = np.hstack((vectors_ordered, vectors[:,i].reshape(4,1)))
+		return [values_ordered, vectors_ordered[:,1:]]
+
+	def eigen_strip_vectors(self, values, vectors, var_min):	
+		var = 0
+		vec = np.empty((4,1), float)
+		for i in range(len(values)):
+			vec = np.hstack((vec, vectors[:,i].reshape(4,1)))
+			var += values[i]
+			if(var/np.sum(values) > var_min):
+				break; 
+		return vec 
+
+	def pca_result(self, data, vectors):
+		return np.matmul(np.matrix.transpose(vectors), np.matrix.transpose(data))
