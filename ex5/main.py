65a8d95a4e56bff6ff8bf75be7c60cc6fc61abab
MarceloBD <marcelodianib@gmail.com>
2018-11-08 17:30:56 -0200

    2d and 3d printing

--- ex5/main.py
+++ ex5/main.py
@@ -17,6 +17,42 @@ colors = {0: 'ro',
           2: 'go'}
 
 if __name__ == '__main__':
+	############################################## 2d
+    data, target = read_file()
+    data = preprocessing.scale(data)
+
+    pca = Pca()
+    cov = pca.cov_matrix(data[:, 0], data[:, 1], data[:, 2], data[:, 3])
+
+    values, vectors = pca.eigen_values_vectors(cov)
+    values, vectors = pca.sort_eigen(values, vectors)
+
+    vectors = pca.eigen_strip_vectors(values, vectors, 0.90)
+
+    print(vectors)
+    values = values[:len(vectors[0])]
+
+    result = np.matrix.transpose(pca.pca_result(data,
+                                 vectors)).reshape(len(data), len(data[0])-2)
+    result[:, 1] = -result[:, 1]
+    points = result
+    count = 0
+    class_points = []
+    fig = plt.figure()
+    for label in range(3):
+        for i in range(len(target)):
+            if(label == target[i]):
+                class_points.append(points[i])
+        class_points = np.array(class_points)
+        x = class_points[:, 0]
+        y = class_points[:, 1]
+        plt.plot(x, y, colors[label], label=str(count))
+        count += 1
+        class_points = []
+    plt.grid()
+    plt.show()
+
+    ############################################## 3d
     data, target = read_file()
     data = preprocessing.scale(data)
 
@@ -47,7 +83,7 @@ if __name__ == '__main__':
         x = class_points[:, 0]
         y = class_points[:, 1]
         z = class_points[:, 2]
-        ax.plot(x, y, z,colors[label], label=str(count))
+        plt.plot(x, y, z, colors[label], label=str(count))
         count += 1
         class_points = []
     plt.grid()
